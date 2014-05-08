e7f81a219a18d9d216f747b790b7c6df10d10118
minsley <insley.matthew@gmail.com>
2014-05-08 10:25:16 -0500

    Completed 161I
    Still has hardcoded input, but I allowed for more jobs than workers, and
    added extra jobs/workers to boost the challenge, as suggested in the
    thread.

--- Challenge161I.cs
+++ Challenge161I.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DailyProg
 {
@@ -12,60 +8,72 @@ namespace DailyProg
     {
         public static void MainMethod()
         {
-            var workers = new Worker[]
+            var workers = new List<Worker>
             {
-                new Worker("Alice", new[] {"Wiring", "Insulation", "Plumbing"}),
-                new Worker("Bob", new[] {"Wiring", "Decoration"}),
-                new Worker("Charlie", new[] {"Wiring", "Plumbing"}),
-                new Worker("David", new[] {"Plumbing"}),
-                new Worker("Erin", new[] {"Insulation", "Decoration", "Finances"}),
+                new Worker("Alice", new List<string>{"GUI", "Backend", "Support"}),
+                new Worker("Bill", new List<string>{"Finances", "Backend"}),
+                new Worker("Cath", new List<string>{"Documentation", "Finances"}),
+                new Worker("Jack", new List<string>{"Documentation", "Frontend", "Support"}),
+                new Worker("Michael", new List<string>{"Frontend"}),
+                new Worker("Steve", new List<string>{"Documentation", "Backend"}),
             };
 
-            var jobs = new Job[]
+            var jobs = new List<Job>
             {
-                new Job("Wiring"),
-                new Job("Insulation"),
-                new Job("Plumbing"),
-                new Job("Decoration"),
+                new Job("GUI"),
+                new Job("Documentation"),
                 new Job("Finances"),
+                new Job("Frontend"),
+                new Job("Backend"),
+                new Job("Support"),
             };
 
-            var jobAssignments = GetWorkers(jobs, workers);
+            workers.Add(new Worker("P1", new List<string> { "J1", "J2" }));
+            workers.Add(new Worker("P2", new List<string> { "J3", "J2" }));
+            workers.Add(new Worker("P3", new List<string> { "J3" }));
+            workers.Add(new Worker("P4", new List<string> { "J4", "J5" }));
+            workers.Add(new Worker("P5", new List<string> { "J4", "J6" }));
+
+            jobs.Add(new Job("J1"));
+            jobs.Add(new Job("J2"));
+            jobs.Add(new Job("J3"));
+            jobs.Add(new Job("J4"));
+            jobs.Add(new Job("J5"));
+            jobs.Add(new Job("J6"));
+
+            AssignWorkers(jobs, workers);
+
+            foreach (var job in jobs)
+            {
+                Console.WriteLine(job.Title + ":\t" + (job.AssignedWorker==null?"":job.AssignedWorker.Name));
+            }
         }
 
-        private static Job[] GetWorkers(Job[] jobs, Worker[] workers)
+        private static void AssignWorkers(List<Job> jobs, List<Worker> workers)
         {
-            // All jobs are filled
-            if (jobs.All(x => x.AssignedWorker != null)) return jobs;
-            else
+            while (workers.Count > 0)
             {
-                // Find least skilled workers
-                int leastSkills = 100;
+                var leastSkills = int.MaxValue;
                 foreach (var worker in workers)
                 {
-                    if (worker.Skills.Count() < leastSkills) leastSkills = worker.Skills.Count();
+                    if (worker.Skills.Count < leastSkills && worker.Skills.Count > 0)
+                        leastSkills = worker.Skills.Count;
                 }
-                for (var i = 0; i < workers.Count(x => x != null); i++)
-                {
-                    if (workers[i].Skills.Count() == leastSkills)
-                    {
-                        foreach (var skill in workers[i].Skills)
-                        {
-                            jobs.Single(x => x.Title == skill).AssignedWorker = workers[i];
-                            workers[i] = null;
-                            GetWorkers(jobs, workers);
-                        }
-                    }
-                }
+
+                var leastSkilled = workers.First(x => x.Skills.Count == leastSkills);
+                var firstSkill = leastSkilled.Skills.First();
+                jobs.Single(x => x.Title == firstSkill).AssignedWorker = leastSkilled;
+                workers.Remove(leastSkilled);
+                workers.ForEach(x => x.Skills.Remove(firstSkill));
             }
         }
 
         private class Worker
         {
             public string Name;
-            public string[] Skills;
+            public List<string> Skills;
 
-            public Worker(string name, string[] skills)
+            public Worker(string name, List<string> skills)
             {
                 Name = name;
                 Skills = skills;
