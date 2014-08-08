0b0f923ad144d6f7806a4350388d4902609f9d0b
minsley <insley.matthew@gmail.com>
2014-08-08 11:02:22 -0500

    Completed 174I
    Ditched the old method, which was needlessly complicated and stupid, in
    favor of SHA1. Much nicer now!

--- Challenge174I.cs
+++ Challenge174I.cs
@@ -1,37 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Text;
+using System.Security.Cryptography;
 
 namespace DailyProg
 {
     class Challenge174I
     {
-        [STAThread]
         public static void MainMethod()
         {
+            const int scaleFactor = 10;
+
             while (true)
             {
                 Console.WriteLine("Enter a username, or 'q' to quit.");
                 var name = Console.ReadLine();
                 if (name == "q") break;
-                var bmp = GetAvatar(name);
+                var bmp = ResizeBitmap(GetAvatar(name),20 * scaleFactor,20 * scaleFactor);
                 bmp.Save("avatar.bmp");
             }
         }
 
         public static Bitmap GetAvatar(string username)
         {
-            var l = username.Length;
-            var bmp = new Bitmap(l,l);
-            for (int i = 0; i < username.Length; i++)
+            var nameBytes = System.Text.Encoding.ASCII.GetBytes(username);
+
+            var sh = SHA1.Create();
+            var h1 = sh.ComputeHash(nameBytes);
+            var h2 = sh.ComputeHash(h1);
+            var h3 = sh.ComputeHash(h2);
+
+            var bmp = new Bitmap(20,20);
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < username.Length; j++)
+                for (int j = 0; j < 10; j++)
                 {
-                    var temp = username[i] + username[j];
-                    var clr = Color.FromArgb(temp/5, temp/5, temp);
-                    bmp.SetPixel(i,j, clr);
+                    var clr = Color.FromArgb(byte.MaxValue, h1[i + j], h2[i + j], h3[i + j]);
+                    bmp.SetPixel(i, j, clr);
+                    bmp.SetPixel(19 - i, j, clr);
+                    bmp.SetPixel(i, 19 - j, clr);
+                    bmp.SetPixel(19 - i, 19 - j, clr);
                 }
             }
             return bmp;
+        }
+
+        // http://stackoverflow.com/a/12522782/1771697
+        private static Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(sourceBMP, 0, 0, width, height);
+            }
+            return result;
         }
     }
 }
